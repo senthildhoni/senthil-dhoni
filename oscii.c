6c5e40eba955f976a5fed71e5ce21b90396d0f49
senthildhoni <35728590+senthildhoni@users.noreply.github.com>
2018-02-04 14:57:47 +0530

    Create oscii.c

---
+++ oscii.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    printf ("enter the value of c");
+    scanf ("%c",&c);
+    ascii(c);
+}
+    void ascii(c) 
+{
+    printf ("the oscii value is%d\n",c);
+    
+    
+    return 0;
+}
