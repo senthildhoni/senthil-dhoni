ac458b84680a602d6f314ae150ccb598dbac6b5b
senthildhoni <35728590+senthildhoni@users.noreply.github.com>
2018-03-15 03:51:18 -0700

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-int main()
+int
+main ()
 {
-int a,b,temp;
-printf("Enter two numbers 'a' and 'b':");
-scanf("%d%d",&a,&b);
-printf("a=%d\nb=%d\n",a,b);
-temp=a;
-a=b;
-b=temp;
-printf("After swapping, the values are:\na=%d\nb=%d\n",a,b);
-return 0;
+  int a, b, t;
+  printf ("Enter two numbers 'a' and 'b':");
+  scanf ("%d%d", &a, &b);
+  printf ("a=%d\nb=%d\n", a, b);
+  t = a;
+  a = b;
+  b = t;
+  printf ("After swapping, the values are:\na=%d\nb=%d\n", a, b);
+  return 0;
 }
